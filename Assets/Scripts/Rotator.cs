4921387d13e0efa0b2e242bcdecef07d73361d6f
vkook2048 <89748029+vkook2048@users.noreply.github.com>
2024-02-26 18:50:43 +0300

    Update Rotator.cs

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    private Vector3 _rotateDirection = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 _rotateDirection;
 
-    void Update()
+    private void Update()
     {
         transform.Rotate(_rotateDirection);
     }
